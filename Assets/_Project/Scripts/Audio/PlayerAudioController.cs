00bec1de64637012538098d79925f83b74db4285
Ale <ale@Mac-mini-di-Ale.local>
2026-06-26 08:11:40 +0200

    Added 2 monobehaviour scripts for handling FMOD audio events - FMOD banks updated with new sfx events - Added .gitattributes to ignore local OS dependent arctifacts

---
+++ Assets/_Project/Scripts/Audio/PlayerAudioController.cs
@@ -0,0 +1,109 @@
+using UnityEngine;
+using FMODUnity;
+
+   public class PlayerAudioController : MonoBehaviour
+    {
+        [SerializeField] private EventReference footStepsSoundEvent;
+        [SerializeField] private EventReference jumpSoundEvent;
+        [SerializeField] private EventReference deathSoundEvent;
+        [SerializeField] private EventReference slipSoundEvent;
+        [SerializeField] private EventReference impactSoundEvent;
+        [SerializeField] private EventReference pickupSoundEvent;
+
+        // 0 = Table - 1 = Garden - 2 = Atomic Dimension
+        private int currentSurfaceType = 0;
+
+        private EventInstance footstepInstance;
+
+        void Start()
+        {
+            if (footstepEvent.IsNull) return;
+            footstepInstance = RuntimeManager.CreateInstance(footstepEvent);
+        }
+
+        public void PlayFootstepFromAnimation()
+        {
+            if (footstepEvent.IsNull) return;
+            footstepInstance.setParameterByName("Surface", currentSurfaceType);
+            footstepInstance.start();
+        }
+
+        public void PlayJumpSound()
+        {
+            if (!jumpSoundEvent.IsNull)
+            {
+                RuntimeManager.PlayOneShot(jumpSoundEvent, transform.position);
+            }
+        }
+
+        public void PlayDeathSound()
+        {
+            if (!deathSoundEvent.IsNull)
+            {
+                RuntimeManager.PlayOneShot(deathSoundEvent, transform.position);
+            }
+        }
+
+        public void PlaySlipSound()
+        {
+            if (!slipSoundEvent.IsNull)
+            {
+                RuntimeManager.PlayOneShot(slipSoundEvent, transform.position);
+            }
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+
+            if (!collision.gameObject.CompareTag("TODO"))
+            {
+                return;
+            }
+
+            if (collision.relativeVelocity.magnitude > 2f)
+            {
+                if (!impactSoundEvent.IsNull)
+                {
+                    Vector3 contactPoint = collision.contacts[0].point;
+                    RuntimeManager.PlayOneShot(impactSoundEvent, contactPoint);
+                }
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("TODO"))
+            {
+                if (!pickupSoundEvent.IsNull)
+                {
+                    RuntimeManager.PlayOneShot(pickupSoundEvent, other.transform.position);
+                }
+
+            }
+
+
+            if (other.CompareTag("TableZone"))
+            {
+                currentSurfaceType = 0;
+            }
+            else if (other.CompareTag("GardenZone"))
+            {
+                currentSurfaceType = 1;
+            }
+            else if (other.CompareTag("AtomicZone"))
+            {
+                currentSurfaceType = 2;
+            }
+
+        }
+
+        void OnDestroy()
+        {
+            if (footstepInstance.isValid())
+            {
+                footstepInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+                footstepInstance.release();
+            }
+        }
+
+    }
