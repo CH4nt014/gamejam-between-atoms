d8a9c4a44ecae26b69eabc4ed52588dbba77ed72
unknown <sishycreates@gmail.com>
2026-06-27 00:01:04 +0200

    Implemented checkpoint and debug respawn

---
+++ Assets/_Project/Scripts/GameManager/Checkpoint/RespawnManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class RespawnManager : MonoBehaviour
+{
+    public static RespawnManager Instance { get; private set; }
+
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform dog;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Respawn()
+    {
+        player.position = CheckpointManager.Instance.PlayerRespawnPosition;
+        dog.position = CheckpointManager.Instance.DogRespawnPosition;
+        dog.GetComponent<DogOffScreenLoseCondition>().ResetTimer();
+        Debug.Log("Respawn!");
+    }
+}
